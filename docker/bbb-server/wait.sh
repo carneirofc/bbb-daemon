60ab9502b457eb1f4d71fe93d7d2d82c190de4f3
Gustavo Ciotto <gustavociotto@gmail.com>
2018-06-05 10:31:57 -0300

    added CHECKOUT_BRANCH to scripts

--- docker/bbb-server/wait.sh
+++ docker/bbb-server/wait.sh
@@ -4,6 +4,11 @@ cd /root/wait-for-it/
 ./wait-for-it.sh -t 0 $REDIS_SERVER_IP:$REDIS_SERVER_PORT
 echo "Redis is UP!"
 cd /root/bbb-daemon/
+
+if [ ${$CHECKOUT_BRANCH+x} ]; then
+	git checkout $CHECKOUT_BRANCH
+fi
+
 git pull
 cd /root/bbb-daemon/server/
 ./run.sh
